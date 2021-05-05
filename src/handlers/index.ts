05c3d5ff3ffa9d9f0e7aaf960b7b8ee58468b6b7
crayola-eater <a-sheikh@outlook.com>
2021-05-05 18:00:44 +0100

    Created 'findDistance' handler function

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,5 +1,8 @@
 import { State } from "../state";
-import { getPostcodeData } from "../util/postcodes";
+import {
+  getPostcodeData,
+  getDistanceBetweenPostcodes,
+} from "../util/postcodes";
 
 export type HandlerArgs = {
   [P: string]: any;
@@ -25,5 +28,19 @@ export const setHome: HandlerWithState = async ({ postcode }, state) => {
 
   return {
     message: `Home location set to ${postcodeData.nuts}`,
+  };
+};
+
+export const findDistance: HandlerWithState = async ({ postcode }, state) => {
+  if (!state?.location?.postcode) {
+    throw new Error(
+      `Failed to find distance as there is no existing location. Did you forget to call: set home <post_code>?`
+    );
+  }
+  const [from, to] = [state.location.postcode, postcode];
+  const distance = await getDistanceBetweenPostcodes(from, to);
+
+  return {
+    message: `Location is ${distance} from home`,
   };
 };
