aa953fb6f964dc3c1596ad7fb3c3f5376414c81a
crayola-eater <a-sheikh@outlook.com>
2021-05-05 12:19:27 +0100

    Created 'getUserInput' function

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -4,3 +4,9 @@ const readline = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+export const getUserInput = (displayText: string) => {
+  return new Promise((resolve) => {
+    readline.question(displayText, resolve);
+  });
+};
