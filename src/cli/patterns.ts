b1b3ec4f76dd3f07038e3063a1eeaffbf9d10866
crayola-eater <a-sheikh@outlook.com>
2021-05-05 18:13:47 +0100

    Fix: bad regex for 'find distance' args

--- src/cli/patterns.ts
+++ src/cli/patterns.ts
@@ -2,7 +2,7 @@ export const patterns = [
   /^(?<cmd>quit)$/i,
   /^(?<cmd>alive)$/i,
   /^(?<cmd>set home) (?<postcode>.+)$/i,
-  /^(?<cmd>find distance) (?<distance>)$/i,
+  /^(?<cmd>find distance) (?<postcode>.+)$/i,
   /^(?<cmd>find directions from) (?<a>.+) to (?<b>.+)$/i,
   /^(?<cmd>set distance to) (?<units>miles|km|radians)$/i,
 ];
