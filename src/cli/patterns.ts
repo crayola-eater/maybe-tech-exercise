c7117902c937638d5e9afdf73c03122c65684512
crayola-eater <a-sheikh@outlook.com>
2021-05-05 18:25:19 +0100

    Renamed args in regex for 'find directions'

--- src/cli/patterns.ts
+++ src/cli/patterns.ts
@@ -3,6 +3,6 @@ export const patterns = [
   /^(?<cmd>alive)$/i,
   /^(?<cmd>set home) (?<postcode>.+)$/i,
   /^(?<cmd>find distance) (?<postcode>.+)$/i,
-  /^(?<cmd>find directions from) (?<a>.+) to (?<b>.+)$/i,
+  /^(?<cmd>find directions) from (?<from>.+) to (?<to>.+)$/i,
   /^(?<cmd>set distance to) (?<units>miles|km|radians)$/i,
 ];
